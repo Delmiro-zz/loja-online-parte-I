09594fe29daa706c600e9a54084ad4ecd720ef96
Diego Delmiro <diegoalbuquerque03@gmail.com>
2019-05-29 17:39:32 -0300

    Exibindo a Capa do Livro na Listagem.

--- src/main/java/br/com/casadocodigo/loja/infra/FileSaver.java
+++ src/main/java/br/com/casadocodigo/loja/infra/FileSaver.java
@@ -1,10 +1,19 @@
 package br.com.casadocodigo.loja.infra;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.OutputStream;
+import java.io.Writer;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+import java.nio.file.Path;
 import javax.servlet.http.Part;
 
 public class FileSaver {
 
-	private static String SERVER_PATH = "/casadocodigo";
+	public static String SERVER_PATH = "/casadocodigo";
 
 	public String write(Part arquivo, String path) {
 		String relativePath = path + "/" + arquivo.getSubmittedFileName();
@@ -12,6 +21,28 @@ public class FileSaver {
 			arquivo.write(SERVER_PATH + "/" + relativePath);
 			return relativePath;
 		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void transfer(Path arquivoFonte, OutputStream outputStream) {
+		try {
+			FileInputStream input = new FileInputStream(arquivoFonte.toFile());
+			try (ReadableByteChannel inputChannel = Channels.newChannel(input);
+				WritableByteChannel outputChannel = Channels.newChannel(outputStream)) {
+				
+				ByteBuffer buffer = ByteBuffer.allocate(1024*10);
+				
+				while (inputChannel.read(buffer) != -1 ) {
+					buffer.flip();
+					outputChannel.write(buffer);
+					buffer.clear();
+				}
+				
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		} catch (FileNotFoundException e) {
 			throw new RuntimeException(e);
 		}
 	}
